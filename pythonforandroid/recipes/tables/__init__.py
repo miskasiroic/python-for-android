48c25c47552aa6b78ad29d9347e7861d13d2ae2b
Miska <Miska.Harjamki@siroic.com>
2023-05-12 10:08:36 +0000

    changes to tables recipe

--- pythonforandroid/recipes/tables/__init__.py
+++ pythonforandroid/recipes/tables/__init__.py
@@ -1,8 +1,9 @@
 from pythonforandroid.recipe import PythonRecipe
 
 class TablesRecipe(PythonRecipe):
+    name = 'tables'
     version = '3.8.0'
     url = 'https://files.pythonhosted.org/packages/bc/42/07b37c0c64a13f005bfe95c8eec5f454fc8dd2caf85fa28add5b2a35b7ab/tables-3.8.0.tar.gz'
-    depends = ['setuptools','wheel','numpy','packaging','py-cpuinfo','Cython','blosc2']
+    depends = ['setuptools','wheel','numpy','packaging','py-cpuinfo','Cython','blosc2 >=2.0.0']
     
 recipe = TablesRecipe()
