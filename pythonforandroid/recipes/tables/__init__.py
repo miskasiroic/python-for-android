beceeed592ea92bcd1c7768b4d6ed7925944287b
Miska <Miska.Harjamki@siroic.com>
2023-05-15 13:40:56 +0000

    1

--- pythonforandroid/recipes/tables/__init__.py
+++ pythonforandroid/recipes/tables/__init__.py
@@ -5,7 +5,7 @@ class TablesRecipe(CythonRecipe):
     version = '3.8.0'
     depends = ['setuptools','wheel','numpy','packaging','py-cpuinfo','Cython','blosc2 >=2.0.0']
     url = 'https://files.pythonhosted.org/packages/bc/42/07b37c0c64a13f005bfe95c8eec5f454fc8dd2caf85fa28add5b2a35b7ab/tables-3.8.0.tar.gz'
-    call_hostpython_via_targetpython = False
-    install_in_hostpython = True
+    call_hostpython_via_targetpython = True
+    install_in_hostpython = False
     
 recipe = TablesRecipe()
